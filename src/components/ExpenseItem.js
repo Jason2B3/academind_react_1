471f116c36cffdb20a918747533176eccf9f62d0
jason <2B3>
2021-07-14 15:39:00 -0400

    commit 38

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate"; // import your sub-component
 function ExpenseItem(props) {
-  const month= props.date.toLocaleString('en-US',{month:'long'})
-  const day= props.date.toLocaleString('en-US',{day:'2-digit'})
-  const year= props.date.getFullYear()
+  // gets props from App.js <ExpenseItem attribs=""/>
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date= {props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
