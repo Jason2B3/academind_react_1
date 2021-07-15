51305d267341765761e9c1d718174034bde642c7
jason <2B3>
2021-07-15 18:51:37 -0400

    commit 47

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,17 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // gets props from App.js <ExpenseItem attribs=""/>
+  let titleVar = props.title; // this orig title comes from App.js
   const clickHandler = () => {
+    titleVar = "UPDATED";
     console.log("I got clicked");
   };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{titleVar}</h2> {/*TITLE GOES HERE*/}
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
