7f477c0d8caef4b35acc8b52f2b546a34ebebddb
jason <2B3>
2021-07-15 23:11:09 -0400

    commit 48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,21 @@
 import ExpenseDate from "./ExpenseDate"; // import your sub-component
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 function ExpenseItem(props) {
-  let titleVar = props.title; // this orig title comes from App.js
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    titleVar = "UPDATED";
-    console.log("I got clicked");
+    setTitle("UPDATED");
+    console.log("inside of handler scope:", title);
+    // after first press, "title" still keeps its orig value
   };
-
+  console.log("in function's root scope:", title); // after first press, it equals UPDATED
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{titleVar}</h2> {/*TITLE GOES HERE*/}
+        <h2>{title}</h2> {/*TITLE GOES HERE*/}
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
