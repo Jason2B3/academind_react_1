0d12f07a7069ed0eca22a9509602c1522d677436
jason <2B3>
2021-07-19 02:40:48 -0400

    commit 59.5: Challenge 2

--- src/components/Expenses/LeanExpenses.js
+++ src/components/Expenses/LeanExpenses.js
@@ -1,10 +1,21 @@
 import ExpenseItem from "./ExpenseItem";
-import './LeanExpenses.css'
-import Card from "../UI/Card"
+import "./LeanExpenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import React, { useState } from "react";
 
 function LeanExpenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterYearHandler = function (yr) {
+    console.log("The year hath arrived up to LeanExpenses.js!", yr);
+    setFilteredYear(yr);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        defaultYear={filteredYear}
+        onSelectFilterYear={filterYearHandler}
+      />
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -28,4 +39,4 @@ function LeanExpenses(props) {
     </Card>
   );
 }
-export default LeanExpenses
+export default LeanExpenses;
